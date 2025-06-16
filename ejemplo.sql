2a0040e9328085d49f3b45cf645d80f0a4b48526
Angie Lilibeth Rios Custodio <134736392+alriosc@users.noreply.github.com>
2025-06-16 11:42:06 -0500

    Update ejemplo.sql

--- ejemplo.sql
+++ ejemplo.sql
@@ -10,7 +10,7 @@ CREATE TABLE cliente (
   direccion                  VARCHAR(50),
   ciudad                     VARCHAR(20),
   provincia                  VARCHAR(15),
-  país                       VARCHAR(20),
+  pais                       VARCHAR(20),
   codigo_postal              VARCHAR(15),
   historial_crediticio       VARCHAR(9),
   ventas_rep_id              VARCHAR(3),
